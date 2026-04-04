ffb4db8982d1f87b8dabda0ce9c24169a39fa54d
Addie <addie2891@gmail.com>
2026-04-04 19:33:15 +0530

    fun dto modified

--- src/main/java/com/codingshutttle/youtube/LearningAnnotations/DTO/FunDto.java
+++ src/main/java/com/codingshutttle/youtube/LearningAnnotations/DTO/FunDto.java
@@ -1,4 +1,8 @@
 package com.codingshutttle.youtube.LearningAnnotations.DTO;
 
 public class FunDto {
+    private Integer number;
+    private Long id;
+    private String email;
+    private String name;
 }
